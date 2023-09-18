28a9f50fd884ea762d7b085266f8aac267636823
ilyas <ilyasgocer@gmail.com>
2023-09-18 19:45:49 +0100

    Test1 updated,GitMenu uploaded

---
+++ src/GitMenu1.java
@@ -0,0 +1,2 @@
+public class GitMenu1 {
+}
--- src/Test1.java
+++ src/Test1.java
@@ -3,5 +3,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("ADDED INTO LOCAL");
+        System.out.println("ADDED INTO LOCAL AND PUSH TO GIT MENU");
     }
 }
